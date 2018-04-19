d369a273f5478105278c8ff8ad154669cfd54c84
Mohamed Romdhani <mromdhani7@gmail.com>
2018-04-19 16:04:18 +0100

    encore

--- src/app/services/posts-list.service.ts
+++ src/app/services/posts-list.service.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/map' ;
 @Injectable()
 export class PostsListService {
 
+
   URL = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private _http: HttpClient) { }
